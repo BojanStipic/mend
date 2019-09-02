82bccf1bac059b27e9c4cea94ff0593e52355289
BojanStipic <bojanstipic@gmail.com>
2019-09-02 18:58:14 +0200

    File opening

--- src/gui.rs
+++ src/gui.rs
@@ -1,7 +1,8 @@
+use std::fs;
 use gtk::prelude::*;
-use gtk::{Application, Builder, Window};
+use gtk::{Application, Builder, Window, FileChooserButton};
 use sourceview::prelude::*;
-use sourceview::{Buffer, StyleSchemeManager};
+use sourceview::{Buffer, LanguageManager, StyleSchemeManager};
 
 pub struct MainWindow {
     ui: Builder,
@@ -33,6 +34,20 @@ impl MainWindow {
     }
 
     fn connect_events(&self) {
+        let file_chooser: FileChooserButton = self.ui.get_object("open_button").unwrap();
+        let ui = self.ui.clone();
+        file_chooser.connect_file_set(move |file_chooser| {
+            file_chooser.get_filename().map(|filename| {
+                let file_contents = fs::read_to_string(&filename).unwrap();
+                let buffer: Buffer = ui.get_object("source_buffer").unwrap();
+                buffer.set_text(&file_contents);
 
+                LanguageManager::get_default().unwrap()
+                    .guess_language(Some(filename.to_str().unwrap()), None)
+                    .map(|lang| {
+                        buffer.set_language(Some(&lang));
+                    });
+            });
+        });
     }
 }
